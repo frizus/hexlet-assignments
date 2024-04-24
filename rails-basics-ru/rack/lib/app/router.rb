1083e5b6fc147c62e595b0c7efb8078ea622ceca
@hexlet/cli <support@hexlet.io>
2024-04-24 16:45:50 +0000

    submit rails-basics-ru/rack

--- rails-basics-ru/rack/lib/app/router.rb
+++ rails-basics-ru/rack/lib/app/router.rb
@@ -7,11 +7,11 @@ class Router
     # BEGIN
     request = Rack::Request.new env
     if request.path == '/'
-      [200, {}, ["Hello, World!"]]
+      [200, {}, ['Hello, World!']]
     elsif request.path == '/about'
-      [200, {}, ["About page"]]
+      [200, {}, ['About page']]
     else
-      [404, {}, ["404 Not Found"]]
+      [404, {}, ['404 Not Found']]
     end
     # END
   end
